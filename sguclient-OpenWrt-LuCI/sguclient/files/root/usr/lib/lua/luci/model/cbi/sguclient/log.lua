1a57fdfb7dc4df7d9864bb90bdcb7e9ecd6eb27a
IDeLoveYou <57473616+IDeLoveYou@users.noreply.github.com>
2022-06-21 22:13:36 +0800

    优化luci界面，向主流插件界面看齐

--- sguclient-OpenWrt-LuCI/sguclient/files/root/usr/lib/lua/luci/model/cbi/sguclient/log.lua
+++ sguclient-OpenWrt-LuCI/sguclient/files/root/usr/lib/lua/luci/model/cbi/sguclient/log.lua
@@ -1,19 +1,20 @@
 require("luci.sys")
 
-m = Map("sguclient", translate("SGUClient Log"), translate("Log file:")
+m = Map("sguclient", "SGUClient Log", translate("Log file:")
         .. [[&nbsp;]]
-        .. translate("/var/log/sguclient.log")
+        .. "/var/log/sguclient.log"
 )
 
-s = m:section(TypedSection, "login", "")  --这里和上面的map一定要能对应到一个已经存在的配置文件的配置字段
+s = m:section(TypedSection, "sguclient")  --这里和上面的map一定要能对应到一个已经存在的配置文件的配置字段
 s.addremove = false
 s.anonymous = true
 view_cfg = s:option(TextValue, "1", nil)
 view_cfg.rows = 25
 view_cfg.readonly = true
 
+
 function view_cfg.cfgvalue()
-    return nixio.fs.readfile("/var/log/sguclient.log") or "-No Log Found-"
+    return nixio.fs.readfile("/var/log/sguclient.log") or "No Log Found"
 end
 
 return m
--- sguclient-OpenWrt-LuCI/sguclient/files/root/usr/lib/lua/luci/model/cbi/sguclient/sguclient.lua
+++ sguclient-OpenWrt-LuCI/sguclient/files/root/usr/lib/lua/luci/model/cbi/sguclient/sguclient.lua
@@ -12,39 +12,33 @@ You may obtain a copy of the License at
 
 require("luci.sys")
 
-m = Map("sguclient", translate("SGUClient LuCI"), translate("ShaoGuan University 3rd Party Network Authentication Client")
+m = Map("sguclient", "SGUClient LuCI", translate("ShaoGuan University 3rd Party Network Authentication Client")
         .. [[<br/>]]
         .. translate("SGUClient is free, You can get the software and source code from")
         .. [[&nbsp;]]
         .. [[<a href="https://github.com/FurryAcetylCoA/sguclient" target="_blank">]]
-        .. translate("Github")
+        .. "Github"
         .. [[</a><br/>]]
         .. translate("QQ Group:")
         .. [[&nbsp;]]
         .. [[<a href="https://jq.qq.com/?_wv=1027&k=C9jldpAy" style='color:red' target="_blank">]]
-        .. translate("638138948")
+        .. "638138948"
         .. [[</a>]]
 )
 
 m:section(SimpleSection).template  = "sguclient/sguclient_status"
 
-s = m:section(TypedSection, "login", "")
+
+s = m:section(TypedSection, "sguclient", translate("General Settings"))
 s.addremove = false
 s.anonymous = true
-
-enable = s:option(Flag, "enable", translate("Enable"), translate("Main control of SGUClient"))
-enable = s:option(Flag, "autoreconnect", translate("Auto Reconnect"), translate("Suggest enable(Otherwise, the program will only be reconnected five times)"))
-enable = s:option(Flag, "noheartbeat", translate("No 1x Heart Beat"), translate("No 802.1x heart beat and cancel alarm(Generally NOT checked)"))
-enable = s:option(Flag, "debug", translate("Debug mode"), translate("Logs will not be cleared automatically in debug mode, and the log will be more detailed(Do not open it for a long time)"))
-
-name = s:option(Value, "username", translate("1x Username"), translate("Fill in your 802.1x username"))
-pass = s:option(Value, "password", translate("1x Password"), translate("Fill in your 802.1x password"))
-pass.password = true
-
+s:option(Flag, "enable", translate("Enable"), translate("Main control of SGUClient"))
+s:option(Value, "username", translate("1x Username"), translate("Fill in your 802.1x username"))
+password = s:option(Value, "password", translate("1x Password"), translate("Fill in your 802.1x password"))
+password.password = true
 isptype = s:option(ListValue, "isptype", translate("ISP Type"), translate("Chose your ISP Type"))
 isptype:value("D", translate("CTCC(DX)"))
 isptype:value("Y", translate("CMCC(YD)"))
-
 ifname = s:option(ListValue, "ifname", translate("AuthInterface"), translate("Chose your authentication interface"))
 for _, network in pairs(luci.sys.net.devices()) do
     if network ~= "lo" then
@@ -56,6 +50,15 @@ for _, network in pairs(luci.sys.net.devices()) do
         end
     end
 end
+
+
+s = m:section(TypedSection, "sguclient", translate("Extra Settings"))
+s.addremove = false
+s.anonymous = true
+s:option(Flag, "autoreconnect", translate("Auto Reconnect"), translate("Suggest enable(Otherwise, the program will only be reconnected five times)"))
+s:option(Flag, "noheartbeat", translate("No 1x Heart Beat"), translate("No 802.1x heart beat and cancel alarm(Generally NOT checked)"))
+s:option(Flag, "debug", translate("Debug mode"), translate("Logs will not be cleared automatically in debug mode, and the log will be more detailed(Do not open it for a long time)"))
+
 
 local apply = luci.http.formvalue("cbi.apply")
 if apply then
