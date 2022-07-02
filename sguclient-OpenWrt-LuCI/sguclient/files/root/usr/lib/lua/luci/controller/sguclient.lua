4dca329a36a4c25a82d720c37baaa9b90e2547c9
IDeLoveYou <57473616+IDeLoveYou@users.noreply.github.com>
2022-07-02 15:51:04 +0800

    更新实时日志功能，再也不用一直刷新页面来更新日志了

--- sguclient-OpenWrt-LuCI/sguclient/files/root/usr/lib/lua/luci/controller/sguclient.lua
+++ sguclient-OpenWrt-LuCI/sguclient/files/root/usr/lib/lua/luci/controller/sguclient.lua
@@ -6,8 +6,9 @@ function index()
     end
     entry({ "admin", "network", "sguclient" }, alias( "admin", "network", "sguclient", "client" ),_("SGUClient LuCI"), 80)
     entry({ "admin", "network", "sguclient", "client" }, cbi("sguclient/sguclient"), _("SGUClient LuCI"), 10)
-    entry({ "admin", "network", "sguclient", "log" }, cbi("sguclient/log"), _("Log"), 20).leaf = true
     entry({ "admin", "network", "sguclient", "status" }, call("act_status")).leaf = true
+    entry({ "admin", "network", "sguclient", "log" }, template("sguclient/sguclient_log"), _("Log"), 20).leaf = true
+    entry({ "admin", "network", "sguclient", "load_log" }, call("load_log")).leaf = true
 end
 
 function act_status()
@@ -16,3 +17,12 @@ function act_status()
     luci.http.prepare_content("application/json")
     luci.http.write_json(e)
 end
+
+function load_log()
+    luci.http.prepare_content("application/json")
+    local log = io.popen("cat /var/log/sguclient.log 2>/dev/null")
+    local content = log:read("*a")
+    luci.http.write(content)
+    log:close()
+end
+
--- sguclient-OpenWrt-LuCI/sguclient/files/root/usr/lib/lua/luci/model/cbi/sguclient/sguclient.lua
+++ sguclient-OpenWrt-LuCI/sguclient/files/root/usr/lib/lua/luci/model/cbi/sguclient/sguclient.lua
@@ -10,8 +10,6 @@ You may obtain a copy of the License at
 	http://www.apache.org/licenses/LICENSE-2.0
 ]]--
 
-require("luci.sys")
-
 m = Map("sguclient", "SGUClient LuCI", translate("ShaoGuan University 3rd Party Network Authentication Client")
         .. [[<br/>]]
         .. translate("SGUClient is free, You can get the software and source code from")
@@ -28,14 +26,12 @@ m = Map("sguclient", "SGUClient LuCI", translate("ShaoGuan University 3rd Party 
 
 m:section(SimpleSection).template  = "sguclient/sguclient_status"
 
-
 s = m:section(TypedSection, "sguclient", translate("General Settings"))
 s.addremove = false
 s.anonymous = true
 s:option(Flag, "enable", translate("Enable"), translate("Main control of SGUClient"))
 s:option(Value, "username", translate("1x Username"), translate("Fill in your 802.1x username"))
-password = s:option(Value, "password", translate("1x Password"), translate("Fill in your 802.1x password"))
-password.password = true
+s:option(Value, "password", translate("1x Password"), translate("Fill in your 802.1x password")).password = true
 isptype = s:option(ListValue, "isptype", translate("ISP Type"), translate("Chose your ISP Type"))
 isptype:value("D", translate("CTCC(DX)"))
 isptype:value("Y", translate("CMCC(YD)"))
@@ -51,14 +47,12 @@ for _, network in pairs(luci.sys.net.devices()) do
     end
 end
 
-
-s = m:section(TypedSection, "sguclient", translate("Extra Settings"))
+s = m:section(NamedSection, "config", "sguclient", translate("Extra Settings"))
 s.addremove = false
 s.anonymous = true
 s:option(Flag, "autoreconnect", translate("Auto Reconnect"), translate("Suggest enable(Otherwise, the program will only be reconnected five times)"))
 s:option(Flag, "noheartbeat", translate("No 1x Heart Beat"), translate("No 802.1x heart beat and cancel alarm(Generally NOT checked)"))
 s:option(Flag, "debug", translate("Debug mode"), translate("Logs will not be cleared automatically in debug mode, and the log will be more detailed(Do not open it for a long time)"))
-
 
 local apply = luci.http.formvalue("cbi.apply")
 if apply then
